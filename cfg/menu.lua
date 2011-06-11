cf5e48e2136b79fc36051675e3711193bb4fe7f9
Cass <kasull88@gmail.com>
2011-06-11 13:03:55 -0400

    Oh right, menu needs to be included for it to work, duh

--- cfg/menu.lua
+++ cfg/menu.lua
@@ -3,14 +3,13 @@ require('freedesktop.utils')
 freedesktop.utils.terminal = terminal
 freedesktop.utils.icon_theme = 'gnome'
 require('freedesktop.menu')
-
 local lock = "slimlock"
 local ck = "dbus-send --system --print-reply --dest=\"org.freedesktop.ConsoleKit\" "
 			.. "/org/freedesktop/ConsoleKit/Manager org.freedesktop.ConsoleKit.Manager."
 local suspend = "dbus-send --system --print-reply --dest=\"org.freedesktop.UPower\" "
 				.. "/org/freedesktop/UPower org.freedesktop.UPower.Suspend"
 
-logoutmenu = {
+local logoutmenu = {
 	{ "Shutdown", ck .. "Stop", freedesktop.utils.lookup_icon({ icon = 'system-shutdown' }) },
 	{ "Reboot", ck .. "Restart",  freedesktop.utils.lookup_icon({ icon = 'gtk-refresh' }) },
 	{ "Suspend", suspend, freedesktop.utils.lookup_icon({ icon = 'media-playback-pause' }) },
--- cfg/wibox.lua
+++ cfg/wibox.lua
@@ -1,7 +1,7 @@
 -- freedesktop menu
 require("vicious")
 --require("delightful.widgets.weather")
---require("cfg.menu")
+require("cfg.menu")
 
 mytextclock = widget({ type = "textbox" })
 vicious.register(mytextclock, vicious.widgets.date, "%a %b %d, %l:%M %p")
