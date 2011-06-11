3818abb9b95d73248417aaebfd45be0fbc66e415
Cass <kasull88@gmail.com>
2011-06-11 12:56:40 -0400

    Don't really need a separate file for the logout menu

--- cfg/menu.lua
+++ cfg/menu.lua
@@ -3,7 +3,20 @@ require('freedesktop.utils')
 freedesktop.utils.terminal = terminal
 freedesktop.utils.icon_theme = 'gnome'
 require('freedesktop.menu')
-require('cfg.logoutmenu')
+
+local lock = "slimlock"
+local ck = "dbus-send --system --print-reply --dest=\"org.freedesktop.ConsoleKit\" "
+			.. "/org/freedesktop/ConsoleKit/Manager org.freedesktop.ConsoleKit.Manager."
+local suspend = "dbus-send --system --print-reply --dest=\"org.freedesktop.UPower\" "
+				.. "/org/freedesktop/UPower org.freedesktop.UPower.Suspend"
+
+logoutmenu = {
+	{ "Shutdown", ck .. "Stop", freedesktop.utils.lookup_icon({ icon = 'system-shutdown' }) },
+	{ "Reboot", ck .. "Restart",  freedesktop.utils.lookup_icon({ icon = 'gtk-refresh' }) },
+	{ "Suspend", suspend, freedesktop.utils.lookup_icon({ icon = 'media-playback-pause' }) },
+	{ "Lock screen", lock, freedesktop.utils.lookup_icon({ icon = 'system-lock-screen' }) },
+	{ "Log out", awesome.quit, freedesktop.utils.lookup_icon({ icon = 'system-log-out' }) },
+}
 
 awful.menu.menu_keys = { up = { "j", "Up" },
 						 down = { "k", "Down" },
