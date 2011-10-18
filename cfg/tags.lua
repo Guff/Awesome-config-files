089c3c14726cf91cab701edb67e4ce340dc04c01
Alfredo Palhares <masterkorp@gmail.com>
2011-10-18 20:15:48 +0100

    Set tiled layout as default on most of tags

--- cfg/tags.lua
+++ cfg/tags.lua
@@ -17,13 +17,13 @@ mytags = { "main", "www", "dev", "doc", "admin", "⚈", "⌘", "⌥" }
 -- All have init = true, because trying to move a client to a not-yet-created
 -- tag seems to be problematic, i.e. takes two attempts
 shifty.config.tags = {
-    [mytags[1]] = { position = 1, init = true, layout = "floating", },
-    [mytags[2]] = { position = 2, init = true, layout = "floating", },
+    [mytags[1]] = { position = 1, init = true, layout = "tile", },
+    [mytags[2]] = { position = 2, init = true, layout = "tile", },
     [mytags[3]] = { position = 3, init = true, layout = "tilebottom",
                     mwfact = 0.7, },
     [mytags[4]] = { position = 4, init = true, layout = "tiletop", },
-    [mytags[5]] = { position = 5, },
-    [mytags[6]] = { position = 6, },
+    [mytags[5]] = { position = 5, layout = "tile" },
+    [mytags[6]] = { position = 6, layout = "tile" },
     [mytags[7]] = { position = 7, mwfact = 0.1943359375, layout = "tileleft", },
     [mytags[8]] = { position = 8, },
     
