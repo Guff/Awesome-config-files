bc5b349691c8a285aa947b6daa0a0fddfeadb69e
Alfredo Palhares <masterkorp@masterkorp.net>
2012-04-23 22:04:27 +0100

    Mplayer2 produces a new window name

--- cfg/tags.lua
+++ cfg/tags.lua
@@ -35,7 +35,7 @@ shifty.config.apps = {
     -- Force luakit on tag 2
     { match = { "luakit" }, tag = mytags[2], },
     -- Make MPlayer float
-    { match = { "MPlayer" }, float = true, }, 
+    { match = { "mplayer2" }, float = true, },
     { match = { "" }, buttons = awful.util.table.join(
         awful.button({ }, 1, function (c) client.focus = c; c:raise() end),
         awful.button({ modkey }, 1, awful.mouse.client.move),
