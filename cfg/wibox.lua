2d84da3985d8fab349f073832a750bb28ba41128
Alfredo Palhares <masterkorp@masterkorp.net>
2013-01-23 14:19:29 +0000

    Remove widget margins.

--- cfg/wibox.lua
+++ cfg/wibox.lua
@@ -118,7 +118,3 @@ end
 
 shifty.taglist = mytaglist
 shifty.promptbox = mypromptbox
-
---awful.widget.layout.margins[cpu_bar.widget] = { left = 5 }
---awful.widget.layout.margins[myweather] = { left = 5 }
---awful.widget.layout.margins[batt_icon] = { left = 2, right = 2 }
