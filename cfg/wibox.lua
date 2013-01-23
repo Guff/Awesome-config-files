da806318d21e28b4ae4a5b1a0a8ba6e0f9267049
Alfredo Palhares <masterkorp@masterkorp.net>
2013-01-23 15:20:58 +0000

    Check the current screen before moving to previous on next tags.

--- cfg/wibox.lua
+++ cfg/wibox.lua
@@ -10,12 +10,12 @@ mylayoutbox = {}
 mytaglist = {}
 
 mytaglist.buttons = awful.util.table.join(
-    awful.button({ }, 1, awful.tag.viewonly),
-    awful.button({ modkey }, 1, awful.client.movetotag),
-    awful.button({ }, 3, awful.tag.viewtoggle),
-    awful.button({ modkey }, 3, awful.client.toggletag),
-    awful.button({ }, 4, awful.tag.viewnext),
-    awful.button({ }, 5, awful.tag.viewprev)
+  awful.button({ }, 1, awful.tag.viewonly),
+  awful.button({ modkey }, 1, awful.client.movetotag),
+  awful.button({ }, 3, awful.tag.viewtoggle),
+  awful.button({ modkey }, 3, awful.client.toggletag),
+  awful.button({ }, 4, function(t) awful.tag.viewnext(awful.tag.getscreen(t)) end),
+  awful.button({ }, 5, function(t) awful.tag.viewprev(awful.tag.getscrren(t)) end)
 )
 mytasklist = {}
 mytasklist.buttons = awful.util.table.join(
