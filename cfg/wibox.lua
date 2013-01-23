fa91b23df6358e1b0b12321c0d034364fe302a49
Alfredo Palhares <masterkorp@masterkorp.net>
2013-01-23 13:25:46 +0000

    Remove hoover calender on the date and time widget

--- cfg/wibox.lua
+++ cfg/wibox.lua
@@ -247,56 +247,6 @@ awful.tooltip({ objects = { volume_icon, }, timer_function = function()
     else return "Volume: " .. get_volume() .. "%" end
 end, timeout = 1 })
 
-local calendar = nil
-local offset = 0
-
-function remove_calendar()
-    if calendar ~= nil then
-        naughty.destroy(calendar)
-        calendar = nil
-        offset = 0
-    end
-end
-
-function add_calendar(inc_offset)
-    local save_offset = offset
-    remove_calendar()
-    offset = save_offset + inc_offset
-    local datespec = os.date("*t")
-    local today = datespec.day
-    datespec = datespec.year * 12 + datespec.month - 1 + offset
-    datespec = (datespec % 12 + 1) .. " " .. math.floor(datespec / 12)
-    local cal = awful.util.pread("cal -m " .. datespec)
-    cal = string.gsub(cal, "^%s*(.-)%s*$", "%1")
-    cal = string.gsub(cal, "([^%d]" .. today .. "[ \n])",
-        '<span bgcolor="white" fgcolor="#61645B">%1</span>', 1)
-    calendar = naughty.notify({
-        text = string.format('<span font_desc="%s">%s</span>', "monospace",
-            os.date("%a, %d %B %Y") .. "\n" .. cal),
-        timeout = 0, hover_timeout = 0.5,
-        width = 160,
-    })
-end
-
-mytextclock:add_signal("mouse::leave", remove_calendar)
-
-mytextclock:buttons(awful.util.table.join(
-    awful.button({ }, 1, function()
-        if calendar ~= nil then
-            remove_calendar()
-        else
-            add_calendar(0)
-        end
-    end),
-    awful.button({ }, 4, function()
-        add_calendar(1)
-    end),
-    awful.button({ }, 5, function()
-        add_calendar(-1)
-    end)
-))
-
-
 -- Create a systray
 mysystray = wibox.widget.systray()
 
