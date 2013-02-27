05fa49f590a4c9b307095e0911a54e341a1c395f
Alfredo Palhares <masterkorp@masterkorp.net>
2013-02-27 22:53:34 +0000

    Set tags on awful only.

--- cfg/tags.lua
+++ cfg/tags.lua
@@ -17,20 +17,7 @@ layouts = {
 
 mytags = { "main", "www", "dev", "doc", "admin", "⚈", "⌘", "⌥" }
 
--- All have init = true, because trying to move a client to a not-yet-created
--- tag seems to be problematic, i.e. takes two attempts
-shifty.config.tags = {
-    [mytags[1]] = { position = 1, init = true, layout = "tile", },
-    [mytags[2]] = { position = 2, layout = "tile", },
-    [mytags[3]] = { position = 3, layout = "tilebottom",
-                    mwfact = 0.7, },
-    [mytags[4]] = { position = 4, layout = "tiletop", },
-    [mytags[5]] = { position = 5, layout = "tile" },
-    [mytags[6]] = { position = 6, layout = "tile" },
-    [mytags[7]] = { position = 7, mwfact = 0.1943359375, layout = "tileleft", },
-    [mytags[8]] = { position = 8, },
-    
-}
-
-local tags = shifty.config.tags
-
+tags = {}
+for s = 1, screen.count() do
+  tags[s] = awful.tag(mytags, s, layouts)
+end
