ad864fffd305e66d0a5cffa76f47180123d0ee41
Alfredo Palhares <masterkorp@masterkorp.net>
2013-03-01 17:30:02 +0000

    Modules can only return one value.

--- cfg/client-bindings.lua
+++ cfg/client-bindings.lua
@@ -1,6 +1,7 @@
 local awful = require("awful")
 
-local clientkeys = awful.util.table.join(
+local clientbinds = {}
+clientbinds.keys = awful.util.table.join(
     awful.key({ modkey,           }, "f",      function (c) c.fullscreen = not c.fullscreen  end),
     awful.key({ modkey, "Shift"   }, "c",      function (c) c:kill()                         end),
     awful.key({ modkey, "Control" }, "space",  awful.client.floating.toggle                     ),
@@ -18,9 +19,9 @@ local clientkeys = awful.util.table.join(
     awful.key({ modkey,           }, "/",      awful.mouse.client.resize)
 )
 
-local clientbuttons = awful.util.table.join(
+clientbinds.buttons = awful.util.table.join(
     awful.button({ }, 1, function (c) client.focus = c; c:raise() end),
     awful.button({ modkey }, 1, awful.mouse.client.move),
     awful.button({ modkey }, 3, awful.mouse.client.resize))
 
-return clientkeys, clientbuttons
+return clientbinds
--- cfg/rules.lua
+++ cfg/rules.lua
@@ -1,7 +1,7 @@
 local awful = require("awful")
 awful.rules = require("awful.rules")
 local beautiful = require("beautiful")
-local clientkeys, clientbuttons = require("cfg.client-bindings")
+local clientbinds = require("cfg.client-bindings")
 
 awful.rules.rules = {
   -- Rules to all the clients
@@ -10,8 +10,8 @@ awful.rules.rules = {
       border_width = beautiful.border_width,
       border_color = beautiful.border_normal,
       focus = awful.client.focus.filter,
-      keys = clientkeys,
-      buttons = clientbuttons
+      keys = clientbinds.keys,
+      buttons = clientbinds.buttons
     }
   },
   -- TODO: Make Mplayer spawn centered
