04e6c09499309914051ded124252891b3219c99e
Alfredo Palhares <masterkorp@masterkorp.net>
2013-03-01 17:35:52 +0000

    Remove global mouse bindings. I do not need that anymore.

--- cfg/global-bindings.lua
+++ cfg/global-bindings.lua
@@ -160,12 +160,3 @@ for i = 1, keynumber do
   )
 end
 root.keys(globalkeys)
-
--- Mouse bindings
-local globalbuttons = awful.util.table.join(
-  awful.button({ }, 3, function () mymainmenu:toggle() end),
-  awful.button({ }, 4, awful.tag.viewnext),
-  awful.button({ }, 5, awful.tag.viewprev)
-)
-root.buttons(globalbuttons)
-
