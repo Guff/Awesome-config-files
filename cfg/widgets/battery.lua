2cc898c6340b298b5077da7088cf46018015a88b
Alfredo Palhares <masterkorp@masterkorp.net>
2014-03-10 14:41:02 +0000

    Battery widget stub.

---
+++ cfg/widgets/battery.lua
@@ -0,0 +1,31 @@
+local awful = require("awful")
+local wibox = require("wibox")
+local naughty = require("naughty")
+
+local bat = {}
+local base_string = "/sys/class/power_supply/BAT0"
+
+local function worker(args)
+  local args = args or {}
+
+  function update()
+    -- Battery status
+    local status = assert(io.open(base_string .. "/status"):read())
+    local charge = assert(io.open(base_string .. "/energy_now"):read("*all"))
+    local capacity = assert(io.open(base_string .. "/energy_full"):read("*all"))
+
+    -- Calculate charge
+    percentage = ((charge * 100) / capacity)
+
+    naughty.notify({ title = "Status", text = status, timeout = 20})
+    naughty.notify({ title = "Charge", text = charge, timeout = 20})
+    naughty.notify({ title = "Capacity", text = capacity, timeout = 20})
+    naughty.notify({ title = "Percentage", text = percentage, timeout = 20})
+
+  end
+    
+  update()
+
+end
+
+return setmetatable(bat, { __call = function(_, ...) return worker(...) end })
