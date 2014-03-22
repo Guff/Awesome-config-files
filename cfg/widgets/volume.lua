c98bfb1aed03687655c1d783d6daed8579632977
Alfredo Palhares <masterkorp@masterkorp.net>
2014-03-22 05:28:22 +0000

    Use the local table was info table.

--- cfg/widgets/volume.lua
+++ cfg/widgets/volume.lua
@@ -2,14 +2,21 @@ local awful = require("awful")
 local wibox = require("wibox")
 local naughty = require("naughty")
 
-local __volume = {}
+local volume = {}
+
+
+volume["high"] = awful.util.getdir("config") .. "/volume-high.png"
+volume["medium"] = awful.util.getdir("config") .. "/volume-medium.png"
+volume["low"] = awful.util.getdir("config") .. "/volume-low.png"
+volume["muted"] = awful.util.getdir("config") .. "/volume-muted.png"
+volume["off"] = awful.util.getdir("config") .. "/volume-off.png"
 
 local function getvolume()
    return string.match(awful.util.pread("amixer -c0 get \"Master\""), "(%d+)%%")
 end
     )
 local function new(args)
-  return __volume = {}
+  return volume = {}
 end
 
 return setmetatable(__bat, { __call = function(_, ...) return new(...) end })
