009887d02763d8478e7ed51e8c4ab494585d0f49
Alfredo Palhares <masterkorp@masterkorp.net>
2014-04-19 02:57:39 +0100

    Update volume icon based on volume.

--- cfg/widgets/volume.lua
+++ cfg/widgets/volume.lua
@@ -12,6 +12,21 @@ volume["low"] = awful.util.getdir("config") .. "/icons/volume-low.png"
 volume["muted"] = awful.util.getdir("config") .. "/icons/volume-muted.png"
 volume["off"] = awful.util.getdir("config") .. "/icons/volume-off.png"
 
+local function get_icon()
+  local percentage = volume:get()
+  if percentage == 0 then
+    icon = volume["off"]
+  elseif percentage < 30 then
+    icon = volume["low"]
+  elseif percentage > 30 and percentage < 70 then
+    icon = volume["medium"]
+  elseif percentage > 70 then
+    icon = volume["high"]
+  end
+  return icon
+end
+
+
 function volume:get()
   return tonumber(string.match(awful.util.pread("amixer -c0 get Master"), "(%d+)%%"))
 end
@@ -28,7 +43,7 @@ function volume:set(increment)
 
   -- Volume notification bar
   -- Passing it trough avoids pop spaming
-  vol_notification = notify:fancy(volume:get(), volume["high"], vol_notification)
+  vol_notification = notify:fancy(volume:get(), get_icon() , vol_notification)
 
   return nil
 end
@@ -46,11 +61,16 @@ local function new(args)
   volume.widget:add(textbox)
 
   local imagebox = wibox.widget.imagebox()
-  imagebox:set_image(volume["high"])
+  imagebox:set_image(get_icon())
   volume.widget:add(imagebox)
 
   local volume_timer = timer ({timeout = 10})
-  volume_timer:connect_signal("timeout", function() textbox:set_text(volume.get()) end)
+  volume_timer:connect_signal("timeout",
+    function()
+      textbox:set_text(volume:get())
+      imagebox:set_image(get_icon())
+    end
+  )
   volume_timer:start()
 
 
