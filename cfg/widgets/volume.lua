6df0a762960a110228a09ebf4b7be6830f5c9232
Alfredo Palhares <masterkorp@masterkorp.net>
2014-07-01 00:43:05 +0100

    Remove the control device out of mute get function.

--- cfg/widgets/volume.lua
+++ cfg/widgets/volume.lua
@@ -13,7 +13,7 @@ volume["muted"] = awful.util.getdir("config") .. "/icons/volume-muted.png"
 volume["off"] = awful.util.getdir("config") .. "/icons/volume-off.png"
 
 local function get_mute()
-  return string.find(awful.util.pread("amixer -c0 get \"Master\""), '%[on%]') == nil
+  return string.find(awful.util.pread("amixer get \"Master\""), '%[on%]') == nil
 end
 
 local function get_icon()
