32383c12bb603e66cd3cbef9c2bd07a5c45b21a3
Beniamin Kalinowski <beniamin.kalinowski@gmail.com>
2019-05-15 16:44:40 +0200

    Brightness widget - changed blocking API to async.

--- cfg/widgets/brightness.lua
+++ cfg/widgets/brightness.lua
@@ -11,30 +11,36 @@ brightness["icon"] = awful.util.getdir("config") .. "/icons/brightness.png"
 local function notify_brightness()
   -- Volume notification bar
   -- Passing it trough avoids pop spaming
-  brightness_notification = notify:fancy(brightness:get(), brightness["icon"] , brightness_notification)
+  brightness:get(function(br_val)  brightness_notification = notify:fancy(br_val, brightness["icon"] , brightness_notification) end ) 
 end
 
-function brightness:get()
-  local brightness = math.floor(tonumber(awful.util.pread("xbacklight -display :0 -get")))
-  if brightness == nil then
-    naughty.notify({
-      text = "Error: Could not get screen light brightness",
-      title = "Brightness"
-    })
-    return 0
-  end
-  return brightness
+function brightness:get(callback)
+  -- local brightness = math.floor(tonumber(awful.util.pread("xbacklight -display :0 -get")))
+  awful.spawn.easy_async("xbacklight -display :0 -get",
+                         function (stdout, stderr, exitreason, exitcode) 
+                             if exitcode == 0
+                             then 
+                               local brightness = math.floor(tonumber(stdout))
+                               callback(brightness)
+                             else
+                                naughty.notify({
+                                  text = "Error: Could not get screen light brightness",
+                                  title = "Brightness"
+                                })
+                              end
+                         end
+                        )
 end
 
 function brightness:up(increment)
-  awful.util.pread("xbacklight -display :0 -inc " .. increment)
+  awful.spawn.spawn("xbacklight -display :0 -inc " .. increment)
   notify_brightness()
 
   return nil
 end
 
 function brightness:down(decrement)
-  awful.util.pread("xbacklight -display :0 -dec " .. decrement)
+  awful.spawn.spawn("xbacklight -display :0 -dec " .. decrement)
   notify_brightness()
 
   return nil
@@ -49,17 +55,17 @@ local function new(args)
   brightness.widget = wibox.layout.fixed.horizontal()
 
   local textbox = wibox.widget.textbox()
-  textbox:set_text(brightness.get())
+  brightness:get(textbox.set_text)
   brightness.widget:add(textbox)
 
   local imagebox = wibox.widget.imagebox()
   imagebox:set_image(brightness["icon"])
   brightness.widget:add(imagebox)
 
-  local brightness_timer = timer ({timeout = 10})
+  local brightness_timer = gears.timer ({timeout = 10})
   brightness_timer:connect_signal("timeout",
     function()
-      textbox:set_text(brightness:get())
+      brightness:get(textbox.set_text)
       imagebox:set_image(brightness["icon"])
     end
   )
